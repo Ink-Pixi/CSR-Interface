fdae9968c466c95b85ed63393810469bc0ff8c79
dhoy <davidh@earthsunmoon.com>
2014-04-16 10:53:58 -0400

    add cnx file

---
+++ CSR Order GUI/cnx.py
@@ -0,0 +1,22 @@
+import mysql.connector
+import pyodbc
+
+class mysql_db():
+    def mysql_connect(self):
+        mysql_db.conn = mysql.connector.connect(user = 'AI_APP', password = 'rowsby01', host = 'wampserver', database = 'xaotees-inkpixi', raise_on_warnings = True) 
+        mysql_db.db = mysql_db.conn.cursor()
+        return mysql_db.db
+    
+    def saleButton(self):
+        sb = mysql_db.mysql_connect(self)
+        sb.execute("""SELECT  ic.inventories_id,ic.inventories_name,ic.inventories_lines_sku, inv.inventories_code,ic.inventories_price, inv.inventories_color, inv.inventories_name as garment_name,ic.inventories_image_url 
+                     FROM inventories_cache ic 
+                     LEFT JOIN inventories inv on inv.inventories_id = ic.inventories_id WHERE ic.on_sale = 1 GROUP BY ic.inventories_lines_sku""")
+        return sb.fetchall()
+
+class mssql_db():
+    def mssql_connect(self):
+        mssql_db.conn = pyodbc.connect('DRIVER={SQL Server}; SERVER=SQLSERVER; DATABASE=ImportExport; UID=ReportCreator; PWD=rowsby01;')
+        mssql_db.db = mssql_db.conn.cursor()
+        return mssql_db.db
+    
